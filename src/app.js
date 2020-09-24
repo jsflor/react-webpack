6e5a311bf581245e37547402b7dfc0b991c8bbf6
Sebastian Flor <sflor@telecomboutique.com>
2020-09-24 17:05:24 +0200

    Added preset for typescript support

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+import Greetings from './hello.tsx';
 import logo from './webpack.png';
 import './app.css';
 
 const App = () => (
     <div>
-        <h1>Greetings</h1>
+        <Greetings/>
         <img src={logo} height={100} width={100} alt={'logo'}/>
     </div>
 );
---
+++ src/hello.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Greetings = () => <h1>Hello from typescript</h1>;
+export default Greetings;
